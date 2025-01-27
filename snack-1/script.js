ec9f8721cc0e6c920ce5f484ceca7aaa0de060c3
chiaralle <chiaraallegrino@gmail.com>
2025-01-27 15:12:07 +0100

    add: snack-1

--- snack-1/script.js
+++ snack-1/script.js
@@ -2,12 +2,13 @@ const names = ['Edoardo', 'Simone', 'Francesco'];
 
 // Stampa in console tutti i nomi
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
+
 let namesString = "";
-names.forEach((element) => namesString += "'"+element+"'");
-/*
-names.forEach(myFunction);
-function myFunction(item) {
-   namesString += "'"+item+"'"
-}*/
+names.forEach((name, index) => { 
+    namesString += `'${name}'`;
+    if(index < names.length -1){
+        namesString += ","
+    } 
+});
 
 console.log(namesString)
