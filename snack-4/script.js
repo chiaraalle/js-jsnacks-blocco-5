d202b85fb39a1b2a14021d7aba78f178f30c52ef
chiaralle <chiaraallegrino@gmail.com>
2025-01-27 16:01:18 +0100

    add: snack-4

--- snack-4/script.js
+++ snack-4/script.js
@@ -7,3 +7,8 @@ const posts = [
 
 // Crea un nuovo array coi nomi di tutti gli autori
 // Risultato: ['Marco', 'Luca', 'Fabrizio', 'Enrico']
+
+ let authors= posts.map( (name) => {
+  return name.author
+ })
+ console.log(authors)
