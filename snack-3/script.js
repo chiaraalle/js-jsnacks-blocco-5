7b0989a86aba38884dea06d32528cdf402d22695
chiaralle <chiaraallegrino@gmail.com>
2025-01-27 15:42:16 +0100

    add: snack-3

--- snack-3/script.js
+++ snack-3/script.js
@@ -2,3 +2,11 @@ const numbers = [2, 8, 4, 7, 2, 87];
 
 // Crea un nuovo array in cui tutti i numeri siano incrementati di 1
 // Risultato: [3, 9, 5, 8, 3, 88]
+
+const increaseNumbers = numbers.map(increase);
+
+function increase(num) {
+  return num + 1;
+}
+
+console.log(increaseNumbers);
